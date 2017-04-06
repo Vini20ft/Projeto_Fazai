f69b8bfceb7af80a6deb718024c2421f25ac1e1e
Caio Ernandes Martins do Nascimento <caio-ernandes@hotmail.com>
2017-04-06 00:04:19 -0300

    Consumo de serviço mockado

--- Mobile/app/src/main/java/fazai/com/br/fazai/activities/EstabelecimentosMapsActivity.java
+++ Mobile/app/src/main/java/fazai/com/br/fazai/activities/EstabelecimentosMapsActivity.java
@@ -1,26 +1,32 @@
 package fazai.com.br.fazai.activities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import fazai.com.br.fazai.R;
+import fazai.com.br.fazai.http.EstabelecimentosTask;
 import fazai.com.br.fazai.model.Estabelecimento;
 
 public class EstabelecimentosMapsActivity extends FragmentActivity implements OnMapReadyCallback, LoaderManager.LoaderCallbacks<List<Estabelecimento>> {
 
-    private GoogleMap mMap;
+    GoogleMap mMap;
+    LoaderManager mLoaderManager;
+    List<Estabelecimento> estabelecimentoList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +36,31 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        if (!verificaConexao()) {
+            Toast.makeText(getApplicationContext(), "Falha na conexão com a internet.",
+                    Toast.LENGTH_LONG).show();
+        }
+
+        mLoaderManager = getSupportLoaderManager();
+        mLoaderManager.initLoader(0, null, this);
+    }
+
+    @Override
+    public Loader<List<Estabelecimento>> onCreateLoader(int id, Bundle args) {
+        return new EstabelecimentosTask(getApplicationContext());
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Estabelecimento>> loader, List<Estabelecimento> data) {
+        if (data != null) {
+            estabelecimentoList = data;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Estabelecimento>> loader) {
+
     }
 
     @Override
@@ -37,35 +68,31 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
         mMap = googleMap;
         LatLng sydney;
         MarkerOptions marker;
+        estabelecimentoList = new ArrayList<>();
 
-        for (int i = 0; i < 5; i++) {
-
-            double latitude = -34 + i;
-            double longitude = 151 + i;
+        for (Estabelecimento estabelecimento : estabelecimentoList) {
+            double latitude = estabelecimento.endereco.localizacao.latitude;
+            double longitude = estabelecimento.endereco.localizacao.longitude;
 
             sydney = new LatLng(latitude, longitude);
-            marker = new MarkerOptions().position(sydney).title("Hello Maps " + i);
+            marker = new MarkerOptions().position(sydney).title(estabelecimento.nome);
 
             mMap.addMarker(marker);
-            mMap.addMarker(marker.position(sydney).title("Marker in Sydney" + i));
+            mMap.addMarker(marker.position(sydney).title(estabelecimento.endereco.rua));
             mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         }
     }
 
-    @Override
-    public Loader<List<Estabelecimento>> onCreateLoader(int id, Bundle args) {
-        return null;
-    }
-
-    @Override
-    public void onLoadFinished(Loader<List<Estabelecimento>> loader, List<Estabelecimento> data) {
-        if (data != null) {
-
+    public  boolean verificaConexao() {
+        boolean conectado;
+        ConnectivityManager conectivtyManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        if (conectivtyManager.getActiveNetworkInfo() != null
+                && conectivtyManager.getActiveNetworkInfo().isAvailable()
+                && conectivtyManager.getActiveNetworkInfo().isConnected()) {
+            conectado = true;
+        } else {
+            conectado = false;
         }
-    }
-
-    @Override
-    public void onLoaderReset(Loader<List<Estabelecimento>> loader) {
-
+        return conectado;
     }
 }
