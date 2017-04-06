e6708f4341eb9d37f4b36de552a5fef22d138805
Caio Ernandes Martins do Nascimento <caio-ernandes@hotmail.com>
2017-04-06 18:52:13 -0300

    Implementação de tooltip do marker

--- Mobile/app/src/main/java/fazai/com/br/fazai/activities/EstabelecimentosMapsActivity.java
+++ Mobile/app/src/main/java/fazai/com/br/fazai/activities/EstabelecimentosMapsActivity.java
@@ -8,6 +8,7 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.widget.Toast;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -21,20 +22,26 @@ import java.util.List;
 import fazai.com.br.fazai.R;
 import fazai.com.br.fazai.http.EstabelecimentosTask;
 import fazai.com.br.fazai.model.Estabelecimento;
+import fazai.com.br.fazai.model.PopupAdapter;
 
-public class EstabelecimentosMapsActivity extends FragmentActivity implements OnMapReadyCallback, LoaderManager.LoaderCallbacks<List<Estabelecimento>> {
+public class EstabelecimentosMapsActivity extends FragmentActivity implements OnMapReadyCallback,
+        LoaderManager.LoaderCallbacks<List<Estabelecimento>>,
+        GoogleMap.OnInfoWindowClickListener {
 
-    GoogleMap mMap;
-    Marker marker;
-
-    LoaderManager mLoaderManager;
-    List<Estabelecimento> estabelecimentoList;
+    private GoogleMap mMap;
+    private Marker marker;
+    private LoaderManager mLoaderManager;
+    private boolean needsInit = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estabelecimentos_maps);
         setUpMap();
+
+        if (savedInstanceState == null) {
+            needsInit = true;
+        }
 
         if (!verificaConexao()) {
             Toast.makeText(getApplicationContext(), "Falha na conexão com a internet.",
@@ -59,8 +66,7 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
     @Override
     public void onLoadFinished(Loader<List<Estabelecimento>> loader, List<Estabelecimento> data) {
         if (data != null) {
-            estabelecimentoList = data;
-            PreencherMarkers(estabelecimentoList);
+            PreencherMarkers(data);
         }
     }
 
@@ -74,7 +80,28 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
         if (googleMap != null) {
             new EstabelecimentosTask(getApplicationContext());
         }
+
+        if (needsInit) {
+            CameraUpdate center=
+                    CameraUpdateFactory.newLatLng(new LatLng(40.76793169992044,
+                            -73.98180484771729));
+            CameraUpdate zoom=CameraUpdateFactory.zoomTo(15);
+
+            googleMap.moveCamera(center);
+            googleMap.animateCamera(zoom);
+        }
+
         mMap = googleMap;
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(), marker.getTitle(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
+
+        //mMap.setInfoWindowAdapter(new PopupAdapter(getLayoutInflater()));
+        mMap.setOnInfoWindowClickListener(this);
     }
 
     private void setUpMap() {
@@ -96,12 +123,15 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
     }
 
     public void PreencherMarkers(List<Estabelecimento> estabelecimentoList) {
-        for (Estabelecimento estabelecimento : estabelecimentoList) {
 
-            long latitude = estabelecimento.endereco.localizacao.latitude;
-            long longitude = estabelecimento.endereco.localizacao.longitude;
+        if (estabelecimentoList != null) {
+            for (Estabelecimento estabelecimento : estabelecimentoList) {
 
-            customAddMarker(new LatLng(latitude, longitude), estabelecimento.nome, estabelecimento.endereco.rua);
+                long latitude = estabelecimento.endereco.localizacao.latitude;
+                long longitude = estabelecimento.endereco.localizacao.longitude;
+
+                customAddMarker(new LatLng(latitude, longitude), estabelecimento.nome, estabelecimento.endereco.rua);
+            }
         }
     }
 
@@ -110,5 +140,11 @@ public class EstabelecimentosMapsActivity extends FragmentActivity implements On
         markerOptions.position(latLng).title(title).snippet(snippet).draggable(true);
 
         marker = mMap.addMarker(markerOptions);
+    }
+
+
+    @Override
+    public void onInfoWindowClick(Marker marker) {
+
     }
 }
