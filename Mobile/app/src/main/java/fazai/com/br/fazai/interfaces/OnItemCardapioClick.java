304cada3070f664edbc994f29ad758c4e162dd34
Vini20ft <vini20ft@hotmail.com>
2017-05-14 22:40:05 -0300

    Mobile removido

--- Mobile/app/src/main/java/fazai/com/br/fazai/interfaces/OnItemCardapioClick.java
+++
@@ -1,8 +0,0 @@
-package fazai.com.br.fazai.interfaces;
-
-import fazai.com.br.fazai.model.ItemCardapio;
-
-
-public interface OnItemCardapioClick {
-    void onItemCardapioClick(ItemCardapio itemCardapio);
-}
